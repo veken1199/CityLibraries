eef5666fb6aa3b8ed3f6664d62620d88019357cf
veken1199 <super_veko@yahoo.com>
2018-06-02 01:11:30 -0400

    Added udm engine

--- engine_mapper.py
+++ engine_mapper.py
@@ -1,6 +1,7 @@
-from Engines import concordia_engine, udm_engine, uqam_engine
+from Engines import concordia_engine, udm_engine, uqam_engine, udm_engine
 
 engine_map = {
     'concordia': concordia_engine.crawl,
-    'uqam' : uqam_engine.crawl
+    'uqam' : uqam_engine.crawl,
+    'udm': udm_engine.crawl
 }
