4b3de04b8dad0d86ba058c6f01a6996baa989478
veken1199 <super_veko@yahoo.com>
2018-06-17 23:56:45 -0400

    Fixed xpath for titles in uqam engine

--- Engines/uqam_engine.py
+++ Engines/uqam_engine.py
@@ -5,7 +5,8 @@ import html5lib
 
 url_uqam = 'http://virtuose.uqam.ca/primo_library/libweb/action/dlSearch.do?institution=UQAM&vid=UQAM&group=GUEST&onCampus=false&fromSitemap=1&lang=fre&search_scope=upac&bulkSize=40&tab=default_tab&indx=1&displayField=title&displayField=creator&query=any,contains,{}'
 result_table_xpath = '//*[@id="exlidResultsTable"]/tbody/tr'
-title_xpath = '//*[@id="exlidResult{}"]/td[4]/div[1]/div/h2/span/text()'
+title_xpath = '//*[@id="exlidResult{}"]/td[4]/div[1]/div/h2/text()'
+composed_title_xpath = '//*[@id="exlidResult{}"]/td[4]/div[1]/div/h2/a/text()'
 author_xpath = '//*[@id="exlidResult{}"]/td[4]/div[1]/div/h3[1]/text()'
 publisher_xpath = '//*[@id="exlidResult{}"]/td[4]/div[1]/div/h3[2]/text()'
 
@@ -18,7 +19,9 @@ def crawl(keyword, results):
     response = []
 
     for i in range(0, len(results_table)):
-        title = ''.join(results_table[i].xpath(title_xpath.format(i)))
+        title = (''.join(results_table[i].xpath(title_xpath.format(i)))).strip()
+        if not title:
+            title = (''.join(results_table[i].xpath(composed_title_xpath.format(i)))).strip()
         author= ''.join(results_table[i].xpath(author_xpath.format(i)))
         publisher = ''.join(results_table[i].xpath(publisher_xpath.format(i)))
 
