0378818f906a7974025c11ace6caa7555ef42b5d
veken1199 <super_veko@yahoo.com>
2018-12-17 23:19:59 -0500

    Cleaning up the directories

--- Engines/engine_config.py
+++
@@ -1,8 +0,0 @@
-urls = {
-    'concordia': "http://encore.concordia.ca/iii/encore/plus/C__S{}__Orightresult__U?lang=eng&suite=def&size=40",
-}
-
-xpath = {
-    'concoridia' : {
-    }
-}
--- engine_mapper.py
+++ engine_mapper.py
@@ -1,4 +1,6 @@
-from engines import concordia_engine, udm_engine, uqam_engine, udm_engine, mcgill_engine
+import mcgill_engine
+from engines import concordia_engine, uqam_engine, udm_engine
+
 engine_map = {
     'concordia': concordia_engine.crawl,
     'uqam' : uqam_engine.crawl,
